1a3d55f12c48af511f9ca3c815b976e467e35607
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-18 09:48:13 -0400

    reset amplify project

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "subsdeliverymanager": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
