6e450ac0f46be30b2cddd18aed826d1be4a83b7a
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-18 17:20:29 -0400

    test login screen chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
 
+import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+
+// Show home page if authenticated
 const Home: NextPage = () => {
   return (
-    <div>
-      <main>Testing testing 123</main>
-    </div>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+
+        <Input
+          placeholder="email@email.com"
+          variant="filled"
+          mb={3}
+          type="email"
+        />
+        <Input placeholder="******" variant="filled" mb={6} type="password" />
+        <Button colorScheme="teal">Log in</Button>
+      </Flex>
+    </Flex>
   );
 };
 
