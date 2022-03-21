b4e317762750bb60c03c780297bf45b80de1f569
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-21 12:26:54 -0400

    modify gql request to user new Cognito auth mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { API } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
 import { listSubscribers } from "../graphql/queries";
 
 const Home: NextPage = () => {
@@ -10,12 +11,9 @@ const Home: NextPage = () => {
   async function fetchSubscribers() {
     const subscriberData = await API.graphql({
       query: listSubscribers,
+      authMode: "AMAZON_COGNITO_USER_POOLS",
     });
     setSubscribers(subscriberData.data.listSubscribers.items);
-  }
-
-  function fetchSubscribersTest() {
-    setSubscribers([{ id: 0, firstName: "Mock Data", lastName: "Last" }]);
   }
 
   useEffect(() => {
@@ -23,15 +21,20 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Subscribers</h1>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <h2>Hello, {user.username}</h2>
+          <h1>Subscribers</h1>
 
-      {subscribers.map((sub) => (
-        <p key={sub.id}>
-          {sub.id} | {sub.lastName}, {sub.firstName}
-        </p>
-      ))}
-    </div>
+          {subscribers.map((sub) => (
+            <p key={sub.id}>
+              {sub.id} | {sub.lastName}, {sub.firstName}
+            </p>
+          ))}
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
