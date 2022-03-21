b3fb8a4fd17fdd61bdbbd6940a818339bc4962b4
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-21 15:27:27 -0400

    reuse top level auth component

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { Authenticator } from "@aws-amplify/ui-react";
-// See min 19:31
+import { AdditionalPageProps } from "./_app";
+import { NextPage } from "next";
 
-function Profile() {
+type PageProps = AdditionalPageProps & {};
+
+const Profile: NextPage<PageProps> = ({ auth }) => {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div>
-          <h1>Profile</h1>
-          <h1>Username: {user.username}</h1>
-          <p>Email: {user.attributes?.email}</p>
-          <button onClick={signOut}>Sign out</button>
-        </div>
-      )}
-    </Authenticator>
+    <div>
+      <h1>Profile</h1>
+      <h1>Username: {auth.user.username}</h1>
+      <p>Email: {auth.user.attributes?.email}</p>
+      <button onClick={auth.signOut}>Sign out</button>
+    </div>
   );
-}
+};
 
 export default Profile;
