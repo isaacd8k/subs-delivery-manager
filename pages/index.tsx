a662196d07203072a96c11894af2eefa7090fbd5
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-21 14:07:23 -0400

    clean up types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,51 @@
 import type { NextPage } from "next";
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { API } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { listSubscribers } from "../graphql/queries";
+import { ListSubscribersQuery, Subscriber } from "../graphql/types";
 
 const Home: NextPage = () => {
-  const [subscribers, setSubscribers] = useState([]);
+  const [subscribers, setSubscribers] = useState<Array<Subscriber> | null[]>(
+    []
+  );
 
   async function fetchSubscribers() {
-    const subscriberData = await API.graphql({
+    const subscriberData = (await API.graphql<ListSubscribersQuery>({
       query: listSubscribers,
       authMode: "AMAZON_COGNITO_USER_POOLS",
-    });
-    setSubscribers(subscriberData.data.listSubscribers.items);
+    })) as { data: ListSubscribersQuery };
+
+    setSubscribers(subscriberData.data.listSubscribers?.items as Subscriber[]);
   }
 
-  useEffect(() => {
-    fetchSubscribers();
-  }, []);
+  // useEffect(() => {
+  //   fetchSubscribers();
+  // }, []);
 
   return (
     <Authenticator>
-      {({ signOut, user }) => (
-        <div>
-          <h2>Hello, {user.username}</h2>
-          <h1>Subscribers</h1>
+      {({ signOut, user }) => {
+        // Since whole app is authenticated, the Authenticator component should
+        // probably exist in the _app.tsx level
+        fetchSubscribers();
 
-          {subscribers.map((sub) => (
-            <p key={sub.id}>
-              {sub.id} | {sub.lastName}, {sub.firstName}
-            </p>
-          ))}
-        </div>
-      )}
+        return (
+          <div>
+            <h2>Hello, {user.username}</h2>
+            <h1>Subscribers</h1>
+
+            {subscribers?.map(
+              (sub) =>
+                sub && (
+                  <p key={sub.id}>
+                    {sub.id} | {sub.lastName}, {sub.firstName}
+                  </p>
+                )
+            )}
+          </div>
+        );
+      }}
     </Authenticator>
   );
 };
