b289f6a19c782df0996d39b570558798f7ee4b3e
Isaac Farciert <rfisaac2296@yahoo.com>
2022-03-25 11:36:40 -0400

    add subscriber delete

--- components/Subscribers/SubscriberViewForm.tsx
+++ components/Subscribers/SubscriberViewForm.tsx
@@ -10,14 +10,25 @@ import {
   FormLabel,
   Heading,
   Input,
-  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spacer,
+  Text,
+  useDisclosure,
+  useToast,
+  VStack,
 } from "@chakra-ui/react";
 
 import type { GetSubscriberQuery, Subscriber } from "../../graphql/types";
 import { API } from "aws-amplify";
 import { getSubscriber } from "../../graphql/queries";
-import { updateSubscriber } from "../../graphql/mutations";
+import { updateSubscriber, deleteSubscriber } from "../../graphql/mutations";
+import { useRouter } from "next/router";
 
 export type Props = {
   subscriberID: string;
@@ -30,6 +41,14 @@ export default function SubscriberViewForm({ subscriberID }: Props) {
   const [isEditMode, setIsEditMode] = useState(false);
   const [updatedFirstName, setUpdatedFirstName] = useState<string>("");
   const [updatedLastName, setUpdatedLastName] = useState<string>("");
+  const toast = useToast();
+  const router = useRouter();
+
+  const {
+    isOpen: isWarningOpen,
+    onOpen: onWarningOpen,
+    onClose: onWarningClose,
+  } = useDisclosure();
 
   async function fetchSubscriberDetails() {
     const subscriber = (await API.graphql<GetSubscriberQuery>({
@@ -85,7 +104,29 @@ export default function SubscriberViewForm({ subscriberID }: Props) {
     // restore state
     if (subscriber) {
       setUpdatedFirstName(subscriber?.firstName);
+      setUpdatedLastName(subscriber?.lastName);
     }
+  }
+
+  async function onDeleteSubscriber() {
+    // call modal
+    await API.graphql({
+      query: deleteSubscriber,
+      variables: { input: { id: subscriberID } },
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    });
+
+    // success
+    toast({
+      title: "Subscriber deleted",
+      position: "top-right",
+      status: "success",
+    });
+
+    // error?
+
+    onWarningClose();
+    router.push("/subscribers");
   }
 
   if (!subscriber) {
@@ -105,49 +146,81 @@ export default function SubscriberViewForm({ subscriberID }: Props) {
           </Heading>
           <Spacer />
 
-          <Link
+          <Button
+            variant="link"
             onClick={() => {
               isEditMode ? onCancelEdit() : setIsEditMode(!isEditMode);
             }}
           >
             {isEditMode ? "Cancel" : "Edit"}
-          </Link>
+          </Button>
         </Flex>
 
         <Divider mt={2} mb={2} />
 
         <Box mb={2}>
           {/* View mode */}
-          <p>Name: {`${subscriber?.firstName} ${subscriber?.lastName}`} </p>
+          <p>Name: {`${subscriber?.firstName} ${subscriber?.lastName}`}</p>
 
           {/* Edit mode */}
           {isEditMode && (
-            <Box>
-              <FormControl>
-                <FormLabel htmlFor="firstName">First name</FormLabel>
-                <Input
-                  id="firstName"
-                  type="text"
-                  value={updatedFirstName}
-                  onChange={(e) => setUpdatedFirstName(e.target.value)}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="lastName">Last name</FormLabel>
-                <Input
-                  id="lastName"
-                  type="text"
-                  value={updatedLastName}
-                  onChange={(e) => setUpdatedLastName(e.target.value)}
-                />
-              </FormControl>
+            <Box
+              ml={4}
+              mt={4}
+              mb={4}
+              p={1}
+              pl={4}
+              borderLeft="1px"
+              borderColor={"gray.600"}
+            >
+              <Text fontSize="xs" color="gray.300">
+                Edit subscriber details
+              </Text>
 
-              <ButtonGroup variant="outline" isAttached>
-                <Button colorScheme="blue" onClick={onSaveEdit}>
-                  Save
+              <VStack spacing={3} mt={2} mb={6}>
+                <FormControl>
+                  <FormLabel htmlFor="firstName">First name</FormLabel>
+                  <Input
+                    id="firstName"
+                    type="text"
+                    value={updatedFirstName}
+                    onChange={(e) => setUpdatedFirstName(e.target.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="lastName">Last name</FormLabel>
+                  <Input
+                    id="lastName"
+                    type="text"
+                    value={updatedLastName}
+                    onChange={(e) => setUpdatedLastName(e.target.value)}
+                  />
+                </FormControl>
+
+                <ButtonGroup variant="outline" isAttached>
+                  <Button colorScheme="blue" onClick={onSaveEdit}>
+                    Save
+                  </Button>
+                  <Button onClick={onCancelEdit}>Cancel</Button>
+                </ButtonGroup>
+              </VStack>
+
+              <Text fontSize="xs" color="gray.300" mt={4}>
+                Delete subscriber
+              </Text>
+              <Box mt={2}>
+                <Text fontSize="sm" mb={2}>
+                  Note: Deleting is permanent. In order to delete, you must
+                  first remove the subscriber from any groups and subscriptions.
+                </Text>
+                <Button
+                  variant="outline"
+                  colorScheme="red"
+                  onClick={onWarningOpen}
+                >
+                  Delete
                 </Button>
-                <Button onClick={onCancelEdit}>Cancel</Button>
-              </ButtonGroup>
+              </Box>
             </Box>
           )}
         </Box>
@@ -162,6 +235,27 @@ export default function SubscriberViewForm({ subscriberID }: Props) {
           <Heading size="sm">Recent orders</Heading>
         </Box>
       </Container>
+
+      <>
+        <Modal onClose={onWarningClose} isOpen={isWarningOpen} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Confirm delete</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              Are you sure you wish to delete? This action is non-reversible.
+            </ModalBody>
+            <ModalFooter>
+              <ButtonGroup>
+                <Button colorScheme="red" onClick={onDeleteSubscriber}>
+                  Delete
+                </Button>
+                <Button onClick={onWarningClose}>Cancel</Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
     </div>
   );
 }
