a9d49a9d96f349be10cc5b1d3f24bdb585c1cdc1
Isaac Farciert <rfisaac2296@yahoo.com>
2022-04-11 09:35:02 -0400

    fix UI and add subscriptions grid

--- components/Periodicals/PeriodicalDetailView.tsx
+++ components/Periodicals/PeriodicalDetailView.tsx
@@ -1,12 +1,25 @@
 import {
+  ArrowForwardIcon,
+  MinusIcon,
+  RepeatClockIcon,
+  RepeatIcon,
+  SettingsIcon,
+  SmallAddIcon,
+  TimeIcon,
+} from "@chakra-ui/icons";
+import {
+  Badge,
   Box,
   Button,
   Container,
   Divider,
   Flex,
+  GridItem,
   Heading,
   Link,
+  SimpleGrid,
   Spacer,
+  Stack,
   Text,
   useDisclosure,
   useToast,
@@ -87,6 +100,7 @@ export default function PeriodicalDetailView({ periodicalID }: Props) {
       </Flex>
 
       <Container maxW="container.md">
+        {/* META */}
         {/* Header */}
         <Flex>
           <Heading size="md">Name: {periodical.name}</Heading>
@@ -101,9 +115,8 @@ export default function PeriodicalDetailView({ periodicalID }: Props) {
           </Button>
         </Flex>
         {/* Recurrence info */}
-        <Text fontSize="sm" color="InactiveCaptionText">
-          {periodical.recurrence}
-        </Text>
+        <Badge colorScheme="green">{periodical.recurrence}</Badge>
+
         {/* Automation settings */}
         <Text fontSize="xs" color="InactiveCaptionText">
           Automation enabled: issues and orders will generate automatically
@@ -111,46 +124,186 @@ export default function PeriodicalDetailView({ periodicalID }: Props) {
 
         <Divider mt={4} mb={4} />
 
-        {/* Issue */}
-        <Flex>
-          <Heading size="md">Issues</Heading>
-          <Spacer />
-          <Button colorScheme="blue" variant="link" fontSize="xs">
-            Actions [menu?]
-          </Button>
-        </Flex>
-        <Box
-          height="36"
-          border="1px solid"
-          borderColor="blue.700"
-          borderRadius="md"
-          mt={2}
-          mb={6}
-        ></Box>
-
-        {/* Subscribers */}
-        <Flex>
-          <Heading size="md">Active subscriptions</Heading>
-          <Spacer />
-          <Button colorScheme="blue" variant="link" fontSize="xs">
-            View all
-          </Button>
-        </Flex>
-        <Box
-          height="36"
-          border="1px solid"
-          borderColor="blue.700"
-          borderRadius="md"
-          mt={2}
-          mb={2}
-        >
-          <Link
-            onClick={() => {
-              onCreateSubscriptionModalOpen();
-            }}
+        {/* ISSUES */}
+        <Box my={6}>
+          <Flex>
+            <Heading size="md">Issues</Heading>
+            <Spacer />
+            <Button colorScheme="blue" variant="link" fontSize="xs">
+              Actions [menu?]
+            </Button>
+          </Flex>
+          <Box
+            border="1px solid"
+            borderColor="blue.700"
+            borderRadius="md"
+            mt={2}
+            mb={2}
           >
-            Add Subscription
-          </Link>
+            {/* Actions pane */}
+            <Box p={2} mb={2} borderBottom="1px solid" borderColor="blue.800">
+              <Stack spacing={4} direction="row" align="center">
+                <Button
+                  leftIcon={<RepeatIcon />}
+                  colorScheme="pink"
+                  variant="ghost"
+                  size="sm"
+                >
+                  Generate issue
+                </Button>
+                <Button
+                  leftIcon={<SettingsIcon />}
+                  colorScheme="pink"
+                  variant="ghost"
+                  size="sm"
+                >
+                  Settings
+                </Button>
+              </Stack>
+            </Box>
+
+            {/* The issues grid */}
+            <SimpleGrid
+              pt={2}
+              spacing={2}
+              templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
+            >
+              {periodical?.issues?.items.map(
+                (issue) =>
+                  issue && (
+                    <Box
+                      borderRadius="lg"
+                      border="1px solid"
+                      borderColor="blue.600"
+                      p={6}
+                      key={issue.id}
+                      sx={{ gridColumnStart: 1 }}
+                    >
+                      <Heading size="sm" isTruncated>
+                        {issue.issueDate}
+                      </Heading>
+
+                      <Text>Status: {issue.status}</Text>
+
+                      <Text>Orders: {issue.orders?.items?.length || "0"}</Text>
+
+                      <Text fontSize="sm">
+                        <Button
+                          leftIcon={<ArrowForwardIcon />}
+                          colorScheme="teal"
+                          variant="ghost"
+                          size="xs"
+                        >
+                          A button
+                        </Button>
+                      </Text>
+                    </Box>
+                  )
+              )}
+            </SimpleGrid>
+
+            {/* No issues */}
+            {(!periodical.issues ||
+              !periodical.issues.items ||
+              periodical.issues.items.length < 1) && (
+              <Box p={2} textAlign="center">
+                No issues.
+              </Box>
+            )}
+          </Box>
+        </Box>
+
+        {/* Subscriptions */}
+        <Box my={6}>
+          <Flex>
+            <Heading size="md">Active subscriptions</Heading>
+            <Spacer />
+            <Button colorScheme="blue" variant="link" fontSize="xs">
+              View all
+            </Button>
+          </Flex>
+          <Box
+            border="1px solid"
+            borderColor="blue.700"
+            borderRadius="md"
+            mt={2}
+            mb={2}
+          >
+            {/* Actions pane */}
+            <Box p={2} mb={2} borderBottom="1px solid" borderColor="blue.800">
+              <Stack spacing={4} direction="row" align="center">
+                <Button
+                  leftIcon={<SmallAddIcon />}
+                  colorScheme="purple"
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => {
+                    onCreateSubscriptionModalOpen();
+                  }}
+                >
+                  New subscription
+                </Button>
+              </Stack>
+            </Box>
+
+            <SimpleGrid
+              spacing={2}
+              templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
+              pt={2}
+            >
+              {periodical?.pubSubscriptions?.items.map(
+                (pubSub) =>
+                  pubSub && (
+                    <Box
+                      borderRadius="lg"
+                      border="1px solid"
+                      borderColor="blue.600"
+                      p={6}
+                      key={pubSub.id}
+                    >
+                      <Heading size="sm" isTruncated>
+                        {pubSub.subscriberID}
+                      </Heading>
+
+                      <Text fontSize="xs">Quantity: {pubSub.qty}</Text>
+                      <Text fontSize="xs">
+                        Created:{" "}
+                        {new Date(pubSub.createdAt).toLocaleDateString()}
+                      </Text>
+
+                      <Box
+                        border="1px solid"
+                        borderColor="gray.700"
+                        borderRadius="md"
+                        mt={2}
+                        mb={2}
+                        p={2}
+                      >
+                        <Heading size="xs">
+                          <TimeIcon />
+                          Pending change:
+                        </Heading>
+                      </Box>
+
+                      <Button
+                        leftIcon={<ArrowForwardIcon />}
+                        colorScheme="teal"
+                        variant="ghost"
+                        size="xs"
+                      >
+                        Edit subscription
+                      </Button>
+                    </Box>
+                  )
+              )}
+
+              {(!periodical.pubSubscriptions ||
+                !periodical.pubSubscriptions.items ||
+                periodical.pubSubscriptions.items.length < 1) && (
+                <Box px={2}>No active subscriptions.</Box>
+              )}
+            </SimpleGrid>
+          </Box>
         </Box>
       </Container>
 
