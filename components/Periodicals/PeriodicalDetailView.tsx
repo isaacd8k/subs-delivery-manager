148b7e2eac26c7a075acc54794c372deb90356e1
Isaac Farciert <rfisaac2296@yahoo.com>
2022-04-21 10:06:42 -0400

    add new upcoming subscriptions skeleton

--- components/Periodicals/PeriodicalDetailView.tsx
+++ components/Periodicals/PeriodicalDetailView.tsx
@@ -16,6 +16,7 @@ import {
   Flex,
   GridItem,
   Heading,
+  HStack,
   Link,
   SimpleGrid,
   Spacer,
@@ -254,69 +255,125 @@ export default function PeriodicalDetailView({ periodicalID }: Props) {
               </Stack>
             </Box>
 
-            <SimpleGrid
-              spacing={2}
-              templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
-              pt={2}
-            >
-              {periodical?.pubSubscriptions?.items.map(
-                (pubSub) =>
-                  pubSub && (
-                    <Box
-                      borderRadius="lg"
-                      border="1px solid"
-                      borderColor="blue.600"
-                      p={6}
-                      key={pubSub.subscriberID + pubSub.periodicalID}
-                    >
-                      <Heading size="sm" isTruncated>
-                        {pubSub.subscriber.firstName}{" "}
-                        {pubSub.subscriber.lastName}
-                      </Heading>
+            {/* Upcoming subscription changes */}
+            <Box p={2}>
+              <Heading size="xs">Upcoming subscription changes</Heading>
+              {/* Grid */}
+              <HStack my={2}>
+                <Box
+                  borderRadius="lg"
+                  border="1px solid"
+                  borderColor="green.700"
+                  p={2}
+                  fontSize="xs"
+                  w="44"
+                >
+                  Name <br />
+                  Current Qty: 3<br />
+                  Pending Qty: 5 (+2)
+                  <br />
+                  Effective: MM-DD-YYYY <br />
+                  Effective status: <Badge colorScheme="green">Ready</Badge>
+                </Box>
+                <Box
+                  borderRadius="lg"
+                  border="1px solid"
+                  borderColor="gray.600"
+                  p={2}
+                  fontSize="xs"
+                  w="44"
+                >
+                  Name <br />
+                  Current Qty: 3<br />
+                  Pending Qty: 1 (-2)
+                  <br />
+                  Effective on: MM-DD-YYYY <br />
+                  Status: <Badge>Pending</Badge>
+                </Box>
+              </HStack>
+            </Box>
 
-                      <Text fontSize="xs">Quantity: {pubSub.qty}</Text>
-                      <Text fontSize="xs">
-                        Created:{" "}
-                        {new Date(pubSub.createdAt).toLocaleDateString()}
-                      </Text>
+            <Divider mt={4} mb={4} />
 
+            {/* Subscriptions */}
+            <Box p={2}>
+              <Heading size="xs">Subscriptions</Heading>
+
+              {/* Subscriptions grid */}
+              <SimpleGrid
+                spacing={2}
+                templateColumns="repeat(auto-fill, minmax(220px, 1fr))"
+                pt={2}
+              >
+                {periodical?.pubSubscriptions?.items.map(
+                  (pubSub) =>
+                    pubSub && (
                       <Box
+                        borderRadius="lg"
                         border="1px solid"
-                        borderColor="gray.700"
-                        borderRadius="md"
-                        mt={2}
-                        mb={2}
-                        p={2}
+                        borderColor="blue.600"
+                        p={6}
+                        key={pubSub.subscriberID + pubSub.periodicalID}
                       >
-                        <Heading size="xs">
-                          <TimeIcon />
-                          Pending change:
+                        <Heading size="sm" isTruncated>
+                          {pubSub.subscriber.firstName}{" "}
+                          {pubSub.subscriber.lastName}
                         </Heading>
+
+                        <Text fontSize="xs">Quantity: {pubSub.qty}</Text>
+                        <Text fontSize="xs">
+                          Created:{" "}
+                          {new Date(pubSub.createdAt).toLocaleDateString()}
+                        </Text>
+
+                        <Box
+                          border="1px solid"
+                          borderColor="gray.700"
+                          borderRadius="md"
+                          mt={2}
+                          mb={2}
+                          p={2}
+                        >
+                          <Heading size="xs">
+                            <TimeIcon />
+                            Pending change:
+                          </Heading>
+                        </Box>
+
+                        <Button
+                          leftIcon={<ArrowForwardIcon />}
+                          colorScheme="teal"
+                          variant="ghost"
+                          size="xs"
+                          onClick={() => {
+                            setSelectedPubSub(pubSub);
+                            onEditPubSubscriptionModalOpen();
+                          }}
+                        >
+                          Edit subscription
+                        </Button>
                       </Box>
+                    )
+                )}
 
-                      <Button
-                        leftIcon={<ArrowForwardIcon />}
-                        colorScheme="teal"
-                        variant="ghost"
-                        size="xs"
-                        onClick={() => {
-                          setSelectedPubSub(pubSub);
-                          onEditPubSubscriptionModalOpen();
-                        }}
-                      >
-                        Edit subscription
-                      </Button>
-                    </Box>
-                  )
-              )}
-
-              {(!periodical.pubSubscriptions ||
-                !periodical.pubSubscriptions.items ||
-                periodical.pubSubscriptions.items.length < 1) && (
-                <Box px={2}>No active subscriptions.</Box>
-              )}
-            </SimpleGrid>
+                {(!periodical.pubSubscriptions ||
+                  !periodical.pubSubscriptions.items ||
+                  periodical.pubSubscriptions.items.length < 1) && (
+                  <Box px={2}>No active subscriptions.</Box>
+                )}
+              </SimpleGrid>
+            </Box>
           </Box>
+        </Box>
+
+        <Box my={6}>
+          <Flex>
+            <Heading size="md">Recently canceled subscriptions</Heading>
+            <Spacer />
+            <Button colorScheme="blue" variant="link" fontSize="xs">
+              View all
+            </Button>
+          </Flex>
         </Box>
       </Container>
 
