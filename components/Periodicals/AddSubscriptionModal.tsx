5366fce1de1c398b7c0a9c00e06beb589c480679
Isaac Farciert <rfisaac2296@yahoo.com>
2022-04-25 11:51:30 -0400

    add datepicker, filter current subscribers

--- components/Periodicals/AddSubscriptionModal.tsx
+++ components/Periodicals/AddSubscriptionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 
 import {
   Modal,
@@ -10,27 +10,24 @@ import {
   HStack,
   FormControl,
   FormLabel,
-  Input,
   ModalFooter,
   Button,
-  FormHelperText,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
   useToast,
+  Text,
 } from "@chakra-ui/react";
-import { AsyncSelect, Select } from "chakra-react-select";
+import { Select } from "chakra-react-select";
 import { API } from "aws-amplify";
-import {
-  ListSubscriberGroupsQuery,
-  ListSubscribersQuery,
-  Periodical,
-  Subscriber,
-} from "../../graphql/types";
+import { ListSubscribersQuery } from "../../graphql/types";
 import { listSubscribers } from "../../graphql/queries";
 import { createPubSubscription } from "../../graphql/mutations";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { GraphQLOptions, GraphQLResult } from "@aws-amplify/api-graphql";
 
 type Props = {
   isOpen: boolean;
@@ -47,6 +44,15 @@ type DropdownOption = {
 
 type DropdownOptions = Array<DropdownOption>;
 
+type ListActiveSubscribersQuery = {
+  listPubSubscriptions: {
+    __typename: "ModelPubSubscriptionConnection";
+    items: Array<{
+      subscriberID: string;
+    }>;
+  };
+};
+
 export default function AddSubscriptionModal({
   isOpen,
   onClose,
@@ -58,29 +64,99 @@ export default function AddSubscriptionModal({
   const [subscriberDropdownOptions, setSubscriberDropdownOptions] =
     useState<DropdownOptions>([]);
   const [subscriptionQty, setSubscriptionQty] = useState("1");
-  const [startDate, setStartDate] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
   const [formIsSubmitting, setFormIsSubmitting] = useState(false);
   const toast = useToast();
 
   // get initial list of subscribers
   useEffect(() => {
-    const fetchSubscribers = async () => {
-      const subscribers = (await API.graphql<ListSubscriberGroupsQuery>({
-        query: listSubscribers,
-        authMode: "AMAZON_COGNITO_USER_POOLS",
-      })) as { data: ListSubscribersQuery } | undefined;
+    const fetchSubscribersQueryObject: GraphQLOptions = {
+      query: listSubscribers,
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    };
+    const fetchActiveSubscriberIdsQueryObject: GraphQLOptions = {
+      query: /* GraphQL */ `
+        query ListActiveSubscribers($periodicalID: ID) {
+          listPubSubscriptions(periodicalID: $periodicalID) {
+            items {
+              subscriberID
+            }
+          }
+        }
+      `,
+      variables: {
+        periodicalID: periodicalID,
+      },
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    };
 
+    const fetchSubscribers = async () => {
+      // immediately dispatch async operations
+      const subscribersPromise = API.graphql(
+        fetchSubscribersQueryObject
+      ) as Promise<GraphQLResult<ListSubscribersQuery>>;
+      const activeSubscriberIdsPromise = API.graphql(
+        fetchActiveSubscriberIdsQueryObject
+      ) as Promise<GraphQLResult<ListActiveSubscribersQuery>>;
+
+      // initialize resolved vars
+      let subscribers: GraphQLResult<ListSubscribersQuery> | null = null;
+      let activeSubscriberIds: GraphQLResult<ListActiveSubscribersQuery> | null =
+        null;
+
+      // await promises and assign values
+      try {
+        [subscribers, activeSubscriberIds] = await Promise.all([
+          subscribersPromise,
+          activeSubscriberIdsPromise,
+        ]);
+      } catch (e) {
+        // toast error
+        toast({
+          title: "Error fetching subscribers. Please try again",
+          position: "top-right",
+          status: "error",
+        });
+      }
+
+      // set state
       if (subscribers?.data?.listSubscribers?.items) {
-        setSubscriberDropdownOptions(
-          // filter out empty subs
-          subscribers.data.listSubscribers.items
-            .filter((sub) => sub !== null)
-            .map((sub) => ({
-              value: sub!.id,
-              id: sub!.id,
-              label: `${sub?.firstName} ${sub?.lastName}`,
-            }))
-        );
+        // new options value
+        let dropdownOptions = subscribers.data.listSubscribers.items
+          // filter null values
+          .filter((sub) => sub !== null)
+          // transform to options value type
+          .map((sub) => ({
+            value: sub!.id,
+            id: sub!.id,
+            label: `${sub?.firstName} ${sub?.lastName}`,
+            isDisabled: false,
+          }));
+
+        // filter out already subscribed
+        // flatten array to array of id strings
+        const activeSubscribers =
+          activeSubscriberIds?.data?.listPubSubscriptions.items?.map(
+            (val) => val.subscriberID
+          );
+        if (activeSubscribers) {
+          dropdownOptions = dropdownOptions.map((val) => {
+            if (activeSubscribers.includes(val.id)) {
+              return {
+                ...val,
+                label: `${val.label} - already subscribed`,
+                isDisabled: true,
+              };
+            }
+            return val;
+          });
+        }
+
+        // set new value
+        setSubscriberDropdownOptions(dropdownOptions);
+      } else {
+        // if no items in fulfilled promise, set value to empty array
+        setSubscriberDropdownOptions([]);
       }
     };
 
@@ -91,7 +167,7 @@ export default function AddSubscriptionModal({
         status: "error",
       });
     });
-  }, [toast]);
+  }, [toast, periodicalID]);
 
   function handleClose() {
     // reset state
@@ -106,13 +182,16 @@ export default function AddSubscriptionModal({
     if (
       !selectedSubscriberOption ||
       !subscriptionQty ||
-      !parseInt(subscriptionQty)
+      !parseInt(subscriptionQty) ||
+      !startDate
     ) {
       return;
     }
 
     const subscriberID = selectedSubscriberOption.id;
     const intQty = parseInt(subscriptionQty);
+    // locale en-CA outputs in desired format YYYY-MM-DD
+    const parsedDate = startDate.toLocaleDateString("en-CA");
 
     // create request
     try {
@@ -123,10 +202,20 @@ export default function AddSubscriptionModal({
             periodicalID: periodicalID,
             subscriberID: subscriberID,
             status: "ACTIVE",
-            qty: intQty,
+            qty: 0,
+            pendingQtyChange: {
+              effectiveDate: parsedDate,
+              qty: intQty,
+            },
           },
         },
         authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+
+      toast({
+        title: "Successfully created subscription",
+        position: "top-right",
+        status: "success",
       });
     } catch (e) {
       return toast({
@@ -139,6 +228,17 @@ export default function AddSubscriptionModal({
     // call success callback
     onSuccess && onSuccess();
   }
+
+  const DatePickerButton = forwardRef<
+    HTMLButtonElement,
+    React.ButtonHTMLAttributes<HTMLButtonElement>
+  >(function DatePickerButton(props, ref) {
+    return (
+      <Button onClick={props.onClick} variant="outline" ref={ref}>
+        {props.value}
+      </Button>
+    );
+  });
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
@@ -156,6 +256,7 @@ export default function AddSubscriptionModal({
                 size="lg"
                 value={selectedSubscriberOption}
                 onChange={(e) => setSelectedSubscriberOption(e)}
+                noOptionsMessage={() => "No available subscribers"}
               />
             </FormControl>
           </HStack>
@@ -180,22 +281,20 @@ export default function AddSubscriptionModal({
             </NumberInput>
           </FormControl>
 
-          {/* Start date -- disabled for now */}
-          {/* <HStack>
-            <FormControl mb={4}>
-              <FormLabel htmlFor="startDate">Start date</FormLabel>
-              <Input
-                id="startDate"
-                type="number"
-                size="lg"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-              <FormHelperText fontSize="xs">
-                If this will take effect on a later date, enter it here.
-              </FormHelperText>
-            </FormControl>
-          </HStack> */}
+          <FormControl mb={4} isRequired>
+            <FormLabel>Effective on:</FormLabel>
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+              customInput={<DatePickerButton />}
+              todayButton="Today"
+            />
+          </FormControl>
+
+          <Text fontSize="xs">
+            New subscriptions are not active by default. Review and activate
+            pending subscriptions in the Upcoming Subscriptions section.
+          </Text>
         </ModalBody>
 
         <ModalFooter>
