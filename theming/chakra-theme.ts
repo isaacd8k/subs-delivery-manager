69f1150c30ab0c75ab6efc10abac2cc97df68a56
Isaac Farciert <rfisaac2296@yahoo.com>
2022-07-04 17:37:18 -0400

    feat: subscribe to system color mode preference changes

--- theming/chakra-theme.ts
+++ theming/chakra-theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "system",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
 const theme = extendTheme({ config });
