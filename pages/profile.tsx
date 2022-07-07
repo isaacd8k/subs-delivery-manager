d918f04d257bd1dce6fd5b20604f4d73ef80c2db
Isaac Farciert <rfisaac2296@yahoo.com>
2022-07-07 10:08:17 -0400

    feat: initialize page styles

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,58 @@
 import React from "react";
 import { AdditionalPageProps } from "./_app";
 import { NextPage } from "next";
+import {
+  Box,
+  Button,
+  Heading,
+  useColorModeValue,
+  Text,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react";
+import { EmailIcon, InfoOutlineIcon, SettingsIcon } from "@chakra-ui/icons";
 
 type PageProps = AdditionalPageProps & {};
 
 const Profile: NextPage<PageProps> = ({ auth }) => {
   return (
     <div>
-      <h1>Profile</h1>
-      <h1>Username: {auth.user.username}</h1>
-      <p>Email: {auth.user.attributes?.email}</p>
-      <button onClick={auth.signOut}>Sign out</button>
+      <Heading size="lg" mb={2}>
+        My Profile
+      </Heading>
+      <Box
+        display="flex"
+        p={2}
+        rounded="md"
+        bgColor={useColorModeValue("gray.100", "gray.700")}
+        alignItems="baseline"
+        gridColumnGap={2}
+      >
+        <InfoOutlineIcon fontSize="sm" />
+        <p>
+          To change your profile information please contact your network
+          administrator
+        </p>
+      </Box>
+
+      <List spacing={2} my={5}>
+        <ListItem>
+          <ListIcon as={SettingsIcon} color={"gray.600"} />
+          Username: <Text as="samp">{auth.user.username}</Text>
+        </ListItem>
+
+        <ListItem>
+          <ListIcon as={EmailIcon} color={"gray.600"} />
+          Email: <Text as="samp">{auth.user.attributes?.email}</Text>
+        </ListItem>
+      </List>
+
+      <Box textAlign="left" mt={4}>
+        <Button colorScheme="blue" onClick={auth.signOut}>
+          Sign out
+        </Button>
+      </Box>
     </div>
   );
 };
